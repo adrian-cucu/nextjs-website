4296e5902e9e8b09d62fcac45775f50a68a28553
Adrian Cucu <adriancucu96@yahoo.com>
2021-02-23 21:31:13 +0200

    Changed hljs to react-syntax-highlighter

--- components/CodeSnipper.js
+++ components/CodeSnipper.js
@@ -1,22 +1,38 @@
-import Highlight from 'react-highlight.js'
+import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter'
+import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript'
+import docco from 'react-syntax-highlighter/dist/cjs/styles/hljs/docco'
+
+function CustomPreTag({ children, _language }) {
+  return (
+    <div className="pre-code-wrapper">
+      <div
+        style={{ position: 'relative', minHeight: '2.5rem', marginTop: '2rem' }}
+      >
+        <div className="pre-language">{_language}</div>
+      </div>
+      <pre className={'hljs language-' + _language}>{children}</pre>
+    </div>
+  )
+}
 
 export default function CodeSnipper({ lang, code, theme }) {
-  const THEMES = [
-    'atom-one-light',
-    'atom-one-dark',
-    'mono-blue',
-    'github',
-    'hybrid',
-    'sunburst',
-  ]
-  const codeTheme = THEMES.includes(theme) ? theme : THEMES[0]
-  // https://highlightjs.org/static/demo/styles/sunburst.css
+  SyntaxHighlighter?.supportedLanguages?.map((lan, inx) => {})
 
   return (
     <>
-      <div>
-        <Highlight language={lang}>{code}</Highlight>
-      </div>
+      <SyntaxHighlighter
+        language={lang}
+        // customStyle={{borderRadius : "1rem"}}
+        // codeTagProps={{language: lang}}
+        useInlineStyles={false}
+        showLineNumbers={true}
+        startingLineNumber={1}
+        PreTag={CustomPreTag}
+        _language={lang}
+        // style={dark}
+      >
+        {code}
+      </SyntaxHighlighter>
     </>
   )
 }
